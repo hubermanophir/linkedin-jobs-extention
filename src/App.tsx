5e26ef4a67a85310447d64826a0ba74543ca9bdf
hubermanophir <ofirhuberman@gmail.com>
2025-02-28 11:45:49 +0200

    implemented basic download file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-// import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-// import Papa from "papaparse";
+import Papa from "papaparse";
 
 interface LinkedinJobElement extends Element {
   dataset?: {
@@ -16,14 +16,34 @@ interface JobsWithDetails {
 }
 
 function App() {
-  const jobsWithDetails: JobsWithDetails[] = [];
+  const [jobsWithDetails, setJobsWithDetails] = useState<JobsWithDetails[]>([]);
+  useEffect(() => {
+    const messageListener = (message: {
+      type: string;
+      data: JobsWithDetails;
+    }) => {
+      console.log({ message });
+      if (message.type === "UPDATE_DATA") {
+        setJobsWithDetails((prevData) => [...prevData, message.data]);
+      }
+    };
+
+    chrome.runtime.onMessage.addListener(messageListener);
+    return () => chrome.runtime.onMessage.removeListener(messageListener);
+  }, []);
+
+  useEffect(() => {
+    console.log({ jobsWithDetails });
+  }, [jobsWithDetails]);
   // const [stopScraper, setStopScraper] = useState(false);
   const onclick = async () => {
     const [tab] = await chrome.tabs.query({ active: true });
     chrome.scripting.executeScript({
       target: { tabId: tab.id! },
-      args: [jobsWithDetails],
-      func: async (jobsWithDetails: JobsWithDetails[]) => {
+      args: [],
+      func: async () => {
+        chrome.runtime.sendMessage("test");
+
         const scrollElementEnd = (scrollableElement: HTMLElement) => {
           scrollableElement.scrollIntoView({
             behavior: "smooth",
@@ -100,8 +120,10 @@ function App() {
                 )
                 ?.textContent?.replace("\n", "")
                 .trim();
-
-              jobsWithDetails.push(data);
+              chrome.runtime.sendMessage({
+                type: "UPDATE_DATA",
+                data,
+              });
               await wait(3000);
             }
           }
@@ -115,19 +137,34 @@ function App() {
         await wait(10000);
         await scrapePage();
         nextButton && nextButton.click();
-
         // } while (nextButton);
-        // console.log("Done");
-        console.log({ jobsWithDetails });
       },
     });
   };
+
+  const downloadCSV = () => {
+    const csv = Papa.unparse(jobsWithDetails);
+    const blob = new Blob([csv], { type: "text/csv" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "data.csv";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <>
       <div>
         <button style={{ width: 150 }} onClick={onclick}>
           Click to get Jobs Ids of page
         </button>
+        {jobsWithDetails && jobsWithDetails.length ? (
+          <button onClick={downloadCSV}>Click to download data</button>
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
